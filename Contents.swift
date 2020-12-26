9ad9f85ff71752a84f70afd5b89a3a19b5f236bf
André <andrevictor50@gmail.com>
2020-12-26 19:55:39 -0300

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let name = "André"
+
+var greetings = "Olá \(name)"
+
+print(greetings)
+
+var shoppingList = ["catfish", "water", "tulips"]
+
+shoppingList[0]
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kell": "Player"
+]
+
+occupations["Kell"]
+
+var emptyArray = [Int]()
+
+emptyArray.append(2)
